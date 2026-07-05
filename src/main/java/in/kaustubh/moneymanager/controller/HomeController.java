d7dcf2552d5b9c80e7ba4918cb4340030d2e031b
Kaustubh-OnPoint <kaustubh.onpoint@gmail.com>
2026-07-05 21:37:52 +0530

    Add root / endpoint for Render health check

--- src/main/java/in/kaustubh/moneymanager/controller/HomeController.java
+++ src/main/java/in/kaustubh/moneymanager/controller/HomeController.java
@@ -1,14 +1,12 @@
 package in.kaustubh.moneymanager.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping({"/status", "/health"})
 public class HomeController {
 
-    @GetMapping
+    @GetMapping({"/", "/status", "/health"})
     public String healthCheck() {
         return "Application is running";
     }
